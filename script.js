e4b0b773702c59f5d11b94f736303a64d650557c
lucas b <lucasborgesbarbosa2012@gmail.com>
2022-03-31 22:19:13 -0300

    I added javascript commands to produce the desire I added javascript commands to produce the desired effect of only one item is marked for each food type.

--- script.js
+++ script.js
@@ -1,13 +1,52 @@
-function border(element) {
-    desabilitar();
-    element.classList.add("contentjs");
+
+function borderPlate(element) {
+    
+    desabilitarPlate();
+    element.classList.add("contentjs-plate");
     element.classList.remove("none")
+    // i = i + 1;
 }
 
-function desabilitar() {
-    const content = document.querySelector(".contentjs")
+function desabilitarPlate() {
+    const content = document.querySelector(".contentjs-plate")
     if (content !== null) {
-        content.classList.remove("contentjs")
+        content.classList.remove("contentjs-plate")
         content.classList.add("none")
     }
 }
+
+function borderDrink(element) {
+    desabilitarDrink();
+    element.classList.add("contentjs-drink");
+    element.classList.remove("none")
+}
+
+function desabilitarDrink() {
+    const content = document.querySelector(".contentjs-drink")
+    if (content !== null) {
+        content.classList.remove("contentjs-drink")
+        content.classList.add("none")
+    }
+}
+
+function borderDessert(element) {
+    desabilitarDessert();
+    element.classList.add("contentjs-dessert");
+    element.classList.remove("none")
+}
+
+function desabilitarDessert() {
+    const content = document.querySelector(".contentjs-dessert")
+    if (content !== null) {
+        content.classList.remove("contentjs-dessert")
+        content.classList.add("none")
+    }
+}
+
+
+
+
+
+
+
+
